445fbd07d6151ce6fdd1238c8625f544cea84fdc
zamcha <caotran293@gmail.com>
2025-11-30 21:40:45 +0700

    fix endpoint

--- src/main/java/vn/tqd/mobilemall/apigateway/ApiGatewayApplication.java
+++ src/main/java/vn/tqd/mobilemall/apigateway/ApiGatewayApplication.java
@@ -2,6 +2,8 @@ package vn.tqd.mobilemall.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.config.CorsRegistry;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -9,5 +11,4 @@ public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
-
 }
